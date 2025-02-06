d733e110678070b7b0da2c5f0ec63ab714dddafd
Patrik <patrik@dbi.nu>
2025-02-06 12:04:25 +0100

    working email soultion

---
+++ server/Mail.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MimeKit;
+
+namespace server;
+
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+public class Mail
+{
+    public void generateNewIssue()
+    {
+        MimeMessage mimeMessage = new MimeMessage();
+            mimeMessage.From.Add(new MailboxAddress("test", "kundtjanstssontest@gmail.com"));
+            
+            
+            // Dynamic email the user enters on the webpage
+            mimeMessage.To.Add(MailboxAddress.Parse("Patrik@dbi.nu"));
+            
+            mimeMessage.Subject = "tjenixen";
+            
+            mimeMessage.Body = new TextPart("plain")
+            {
+                Text = @"asdasdasdasdasda"
+            };
+            
+            SmtpClient client = new SmtpClient();
+            try
+            {
+                client.Connect("smtp.gmail.com", 465, true);
+                client.Authenticate("kundtjanstssontest@gmail.com", "pfbq mnjw ifry qdyh");
+                client.Send(mimeMessage);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+            }
+    }
+}
+
+
+
