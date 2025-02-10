3ecff0a88e3f247b664e1e7c3b9914982f4ac67e
Ranine Husky <max.svennerheim@gmail.com>
2025-02-10 13:35:16 +0100

    added env variables for each relevant part for connection

--- server/Database.cs
+++ server/Database.cs
@@ -5,12 +5,21 @@ using DotNetEnv;
 public class Database
 {
 
+    // create a .env file in /server and add these rows with replacement for relevant local data
+    //
+    // PGHOST=localhost
+    // PGPORT=myport
+    // PGDATABASE=mydb
+    // PGUSER=myuser
+    // PGPASSWORD=mypassword
+    //
+    // for testing with local db
     
-    private readonly string _host = "localhost";
-    private readonly string _port = "5432";
-    private readonly string _username = "postgres";
-    private readonly string _password; // create a .env file in /server and add this row PGPASSWORD=mypassword for testing with local db
-    private readonly string _database = "postgres";
+    private readonly string _host;
+    private readonly string _port;
+    private readonly string _username;
+    private readonly string _password;
+    private readonly string _database;
 
     private NpgsqlDataSource _connection;
 
@@ -24,7 +33,12 @@ public class Database
     public Database()
     {
         Env.Load();
-        _password = Environment.GetEnvironmentVariable("PGPASSWORD") ?? "didn't load correctly";
+        _host = Environment.GetEnvironmentVariable("PGHOST") ?? "no host found";
+        _port = Environment.GetEnvironmentVariable("PGPORT") ?? "no port found";
+        _username = Environment.GetEnvironmentVariable("PGUSER") ?? "no user found";
+        _password = Environment.GetEnvironmentVariable("PGPASSWORD") ?? "no pw found";
+        _database = Environment.GetEnvironmentVariable("PGDATABASE") ?? "no db found";
+        
         string connectionString = $"Host={_host};Port={_port};Username={_username};Password={_password};Database={_database}";
         _connection = NpgsqlDataSource.Create(connectionString);
         
