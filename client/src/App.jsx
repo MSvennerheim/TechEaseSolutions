823d5855c9e58a0e5e09ac777d35caf234b4af7e
Markus Olsson <markus.olsson2006@gmail.com>
2025-02-10 13:13:05 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,50 @@
 import { Navbar, Nav } from 'rsuite';
+import { BrowserRouter, Route, Router, Routes } from "react-router";
 import CogIcon from '@rsuite/icons/legacy/Cog';
 import './index.css';
+import Adminsida from "./adminsida";
+import Inloggningssida from "./inloggningssida";
+import KontaktaOss from "./kontaktaoss";
+import Arbetarsida from "./arbetarsida";
+import ConfirmationSida from "./confirmationsida";
+import Redigeramall from "./redigeramall";
+import Redigeramedarbetare from "./redigerarmedarbetare";
+
+function App() {
+  return
+  <Router>
+<Routes>
+      <Route path="/inloggning" element={<Inloggningssida />} />
+      {user?.role === 'admin' && (
+        <>      
+          <Route path="/admin" element={<Adminsida />} />
+          <Route path="/Redigerarmedarbetare" element={<Redigerarmedarbetare />} />
+          <Route path="/Redigeramall" element={<Redigeramall />} />
+     </>
+      )}
+<Route path="/admin" element={<Adminsida />} />
+<Route path="/confirmation" element={<ConfirmationSida />} />
+<Route path="/Kontakta oss" element={<KontaktaOss />} />
+<Route path="/arbetarsida" element={<Arbetarsida />} />
+<Route path="/Redigerarmedarbetare" element={<Redigerarmedarbetare/>} />
+<Route path="/Redigeramall" element={<Redigeramall/>} />      
+
+
+</Routes>
+</Router>
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 const App = () => (
   <Navbar>
     <Navbar.Brand href="#">TechEaseSolutions</Navbar.Brand>
@@ -21,4 +65,5 @@ const App = () => (
       <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
     </Nav>
   </Navbar>
-);
+  );
+*/
