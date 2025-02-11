062b38f90c811f984f7180aebf6da6b156da9400
Ranine Husky <max.svennerheim@gmail.com>
2025-02-11 11:19:09 +0100

    constructed query for JSON object of chat history

--- server/Queries.cs
+++ server/Queries.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
 namespace server;
 using Npgsql;
 
@@ -9,5 +12,33 @@ public class Queries
     {
         _db = db;
     }
+
+    [HttpGet("GetChatHistory")]
+    public async Task<string> GetChatHistory(int chat)
+    {
+        
+        // get chat history for a specific chat using chatid as a JSON file
+        
+        var messages = new List<object>();
+        
+        await using (var cmd = _db.CreateCommand("SELECT message, email, timestamp FROM messages JOIN users ON messages.sender = users.id WHERE chatid = @chatid"))
+        {
+            cmd.Parameters.AddWithValue("@chatid", chat);
+            await using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    messages.Add(new
+                    {
+                        message = reader.GetString(0),
+                        sender = reader.GetString(1),
+                        timestamp = reader.GetDateTime(2).ToString("o")
+                    });
+                }
+            }
+        }
+        return JsonSerializer.Serialize(messages, new JsonSerializerOptions {WriteIndented = true});
+    }
+    
     
 }
