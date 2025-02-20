f0a00b9e7c4fe5b77a5b104efec8370e530376e8
Markus Olsson <markus.olsson2006@gmail.com>
2025-02-20 11:59:43 +0100

    idk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './Pages/LoginUI.jsx';
-
 import Adminsida from "./Pages/Adminsida.jsx";
 import Arbetarsida from "./Pages/Arbetarsida.jsx";
 import KontaktaOss from "./Pages/Kontaktaoss.jsx";
