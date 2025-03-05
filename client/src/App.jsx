4c2d8aab5f2b91503e888245f7250c6341f721cb
Markus Olsson <markus.olsson2006@gmail.com>
2025-03-05 11:10:39 +0100

    ccskontaktaoss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './Pages/LoginUI.jsx';
 
 import Adminsida from "./Pages/Adminsida.jsx";
-import Arbetarsida from "./Pages/Arbetarsida.jsx";
+import Arbetarsida from "./Pages/arbetarsida.jsx";
 import KontaktaOss from "./Pages/Kontaktaoss.jsx";
 import Confirmationsida from "./Pages/confirmationsida.jsx";
 import Redigeramall from "./Pages/redigeramall.jsx";
@@ -20,15 +20,12 @@ export default function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/confirmation" element={<Confirmationsida />} />
-
         <Route path="/kontaktaoss/:companyName" element={<KontaktaOss />} />
-
         <Route path="/kontaktaoss" element={<KontaktaOss />} />
         {/* Skyddad routes */}
         <Route path="/arbetarsida/:company" element={<ProtectedRoute><Arbetarsida /></ProtectedRoute>} />
         <Route path="/admin/:company" element={<ProtectedRoute adminOnly={true}><Adminsida /></ProtectedRoute>} />
         {/* vet inte rikigt än Routes */}
-
         <Route path="/redigeramedarbetare" element={<Redigeramedarbetare />} />
         <Route path="/redigeramall" element={<Redigeramall />} />
         <Route path="Chat/:chatId" element={<ChatHistory />} />
