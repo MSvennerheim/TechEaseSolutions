8140669d9023095d334a9e7a68335de6e146364a
Markus Olsson <markus.olsson2006@gmail.com>
2025-03-07 11:32:20 +0100

    passwordreset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,8 @@ import Adminsida from "./Pages/Adminsida.jsx";
 import Arbetarsida from "./Pages/Arbetarsida.jsx";
 import KontaktaOss from "./Pages/kontaktaoss.jsx";
 import Confirmationsida from "./Pages/confirmationsida.jsx";
-import CaseEditor from "./Pages/CaseEditor.jsx";
 import ChatHistory from "./pages/Chat.jsx"
+import PasswordresetForm from "./Pages/PasswordresetForm.jsx";
 import EditForm from "./pages/CaseEditor.jsx"
 import './index.css';
 import ProtectedRoute from './Components/ProtectedRoute.jsx';
@@ -21,7 +21,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/confirmation" element={<Confirmationsida />} />
         <Route path="/kontaktaoss/:companyName" element={<KontaktaOss />} />
-
+        <Route path="/reset-password" element={<PasswordresetForm />} />
         {/* Skyddad routes */}
         <Route path="/Arbetarsida" element={<ProtectedRoute><Arbetarsida /></ProtectedRoute>} />
         <Route path="/admin" element={<ProtectedRoute adminOnly={true}><Adminsida /></ProtectedRoute>} />
